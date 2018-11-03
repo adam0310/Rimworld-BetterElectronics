fb4e21acbffd469a6c4cb2b6af8a1193141b9a99
Adam Bucior <35536269+adam0310@users.noreply.github.com>
2018-11-03 18:16:17 +0100

    First Commit

---
+++ Source/DisableRandomBreakdowns/NoBreakdowns.cs
@@ -0,0 +1,31 @@
+﻿using Verse;
+using RimWorld;
+using HugsLib;
+using HugsLib.Settings;
+using Harmony;
+
+namespace BetterElectronics
+{
+    [StaticConstructorOnStartup]
+    public class BetterElectronics : ModBase
+    {
+        public override string ModIdentifier => "BetterElectronics";
+
+        public static SettingHandle<bool> no_breakdowns_enabled;
+
+
+        public override void Initialize()
+        {
+            no_breakdowns_enabled = Settings.GetHandle<bool>("no_breakdown_enabled", "NoBreakdowns.Enabled".Translate(), /*"You can disable random breakdown disabler just incase it causes problem or incompatibility."*/"NoBreakdowns.Enabled.Alt".Translate(), true);
+        }
+    }
+
+    [HarmonyPatch(typeof(CompBreakdownable), "CheckForBreakdown")]
+    public static class Patch_NoBreakdown
+    {
+        public static bool Prefix()
+        {
+            return !BetterElectronics.no_breakdowns_enabled;
+        }
+    }
+}
